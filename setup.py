ca8206809f3a121655ef768e86d27d679cc386fc
Eric V. Smith <eric@trueblade.com>
2014-02-11 17:06:06 -0500

    Fixed non-RPM name. Added a comment about the package name hack.

--- setup.py
+++ setup.py
@@ -2,10 +2,19 @@ from __future__ import print_function
 from distutils.core import setup, Command
 import sys
 
+# This is a hack in order to get the package name to be different when
+#  building an RPM file. When 'setup.py bdist_rpm' is called, it invokes
+#  setup.py twice more, with these command lines:
+# ['setup.py', 'build']
+# ['setup.py', 'install', '-O1', '--root=/home/eric/local/toposort/build/bdist.linux-i686/rpm/BUILDROOT/python-toposort-0.1-1.i386', '--record=INSTALLED_FILES']
+# It's only on the original call (when bdist_rpm is in sys.argv) that
+#  I adjust the package name. With Python 2.7, that's enough. I'm not
+#  sure about 3.x.
+
 if 'bdist_rpm' in sys.argv:
     name = 'python-toposort'
 else:
-    name = 'python'
+    name = 'toposort'
 
 # run our tests
 class PyTest(Command):
